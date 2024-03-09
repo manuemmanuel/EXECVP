90e639af45fd8f579d2ca93e8f038865935cd0c7
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2024-03-10 00:23:15 +0530

    Update first.c

--- first.c
+++ first.c
@@ -7,16 +7,3 @@ char *args[]={"./ex",NULL};
 execvp(args[0],args);
 printf("End");
 }
-new.c
-#include<stdio.h>
-#include<unistd.h>
-int main()
-{
-int i;
-printf("I am the program called by the first process");
-for(i=1;i<10;i++)
-{
-printf("%d*%d\n",i,i);
-}
-return 0;
-}
